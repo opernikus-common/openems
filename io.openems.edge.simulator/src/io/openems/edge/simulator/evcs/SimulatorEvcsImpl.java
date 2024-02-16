4ff08167dfe04bdebc36e29b63362b74b89dcad2
Thomas Sicking <91258335+tsicking@users.noreply.github.com>
2024-02-16 15:36:03 +0100

    Duplicated super.handleEvent() removed

--- io.openems.edge.simulator/src/io/openems/edge/simulator/evcs/SimulatorEvcsImpl.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/evcs/SimulatorEvcsImpl.java
@@ -128,7 +128,6 @@ public class SimulatorEvcsImpl extends AbstractManagedEvcsComponent
 		if (!this.isEnabled()) {
 			return;
 		}
-		super.handleEvent(event);
 		switch (event.getTopic()) {
 		case EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE -> {
 			this.updateChannels();
